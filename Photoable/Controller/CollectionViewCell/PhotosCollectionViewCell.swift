305e9a817cb8a0a09db10c003b199098bc62bc72
lcy <pgbenny0035@gmail.com>
2023-02-01 18:58:06 +0900

    Feat: Collection VIew로 사진들 화면에 띄우기 구현

---
+++ Photoable/Controller/CollectionViewCell/PhotosCollectionViewCell.swift
@@ -0,0 +1,47 @@
+//
+//  PhotosCollectionViewCell.swift
+//  Photoable
+//
+//  Created by 임채윤 on 2023/02/01.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    static let identifier = "PhotosCollectionViewCell"
+    var representedAssetIdentifier: String?
+    
+    let image: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .gray
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+       super.init(frame: frame)
+       self.setUILayout()
+   }
+   
+   required init?(coder: NSCoder) {
+       fatalError("init(coder:) has not been implemented")
+   }
+    
+    private func setUILayout() {
+        let views = [image]
+        
+        views.forEach { view in
+            view.translatesAutoresizingMaskIntoConstraints = false
+            self.addSubview(view)
+        }
+        
+        NSLayoutConstraint.activate([
+            image.topAnchor.constraint(equalTo: self.topAnchor),
+            image.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            image.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            image.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+        ])
+    }
+}
--- Photoable/Controller/PhotosViewController.swift
+++ Photoable/Controller/PhotosViewController.swift
@@ -10,11 +10,13 @@ import PhotosUI
 
 class PhotosViewController: UIViewController {
 
-    var photos = [UIImage]()
+    var photos = PHFetchResult<PHAsset>()
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setUILayout()
         pickPhoto()
+        configurationCollectionView()
         // Do any additional setup after loading the view.
     }
     
@@ -37,31 +39,96 @@ class PhotosViewController: UIViewController {
         getPermissionIfNecessary { permission in
             if permission {
                 let allPhotosOptions = PHFetchOptions()
+                
                 allPhotosOptions.sortDescriptors = [
                     NSSortDescriptor(key: "creationDate", ascending: false)
                 ]
                 
-                let allPhotos = PHAsset.fetchAssets(with: allPhotosOptions)
+                self.photos = PHAsset.fetchAssets(with: allPhotosOptions)
                 
-                allPhotos.enumerateObjects { (asset, count, stop) in
-                    let targetSize = CGSize(width: UIScreen.main.bounds.width, height: CGFloat(asset.pixelHeight) * UIScreen.main.bounds.width / CGFloat(asset.pixelWidth))
-                    let imageManager = PHImageManager.default()
-                    let options = PHImageRequestOptions()
-                    options.isSynchronous = true
-                    imageManager.requestImage(for: asset, targetSize: targetSize, contentMode: PHImageContentMode.aspectFit, options: options, resultHandler: { [weak self] (image, info) in
-                        if let image = image {
-                            self?.photos.append(image)
-                        }
-                    })
-                    print(count)
-                }
-                print(self.photos)
+                self.photosCollectionView.reloadData()
             } else {
                 // 권한이 허용되지 않았다고 alert 띄우기
             }
         }
-        
-        
     }
 
+    private func configurationCollectionView() {
+        photosCollectionView.delegate = self
+        photosCollectionView.dataSource = self
+        let cellIdentifier = PhotosCollectionViewCell.identifier
+        photosCollectionView.register(PhotosCollectionViewCell.self, forCellWithReuseIdentifier: cellIdentifier)
+    }
+    
+    let photosCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        return collectionView
+    }()
+    
+    
+    private func setUILayout() {
+        let views = [photosCollectionView]
+        
+        views.forEach { view in
+            view.translatesAutoresizingMaskIntoConstraints = false
+            self.view.addSubview(view)
+        }
+        
+        print("====== Set UI layout ======")
+        NSLayoutConstraint.activate([
+            photosCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            photosCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            photosCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            photosCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+}
+
+extension PhotosViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        print("=========== Photos Count == \(photos.count) =================")
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cellIdentifier = PhotosCollectionViewCell.identifier
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as? PhotosCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+
+        let imageManager = PHCachingImageManager()
+        let asset = self.photos[indexPath.item]
+        let thumbnailSize = CGSize(width: 1024 * UIScreen.main.scale, height: 1024 * UIScreen.main.scale)
+        cell.representedAssetIdentifier = asset.localIdentifier
+        
+        imageManager.requestImage(for: asset, targetSize: thumbnailSize, contentMode: .aspectFill, options: nil, resultHandler: { image, _ in
+            if cell.representedAssetIdentifier == asset.localIdentifier {
+                DispatchQueue.main.async {
+                    cell.image.image = image
+                }
+            }
+        })
+        
+        return cell
+    }
+}
+
+extension PhotosViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width / 4 - 1
+        let height = width
+        let size = CGSize(width: width, height: height)
+        
+        return size
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
 }
